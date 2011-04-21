ba50988555c767a256aeb96ef2c21fa857baa583
Andrea Franz <andrea@gravityblast.com>
2011-04-21 18:00:44 +0200

    powerman 50000

--- lib/rack/o.rb
+++ lib/rack/o.rb
@@ -1,6 +1,6 @@
 module Rack
   class O
-    JS = %|<script type="text/javascript" charset="utf-8">(function(){var c=[],b="38,38,40,40,37,39,37,39,66,65";var a=function(f){c.push(f.keyCode);if(c.toString().indexOf(b)>=0){var d=document.createElement("img");d.style="position:absolute; bottom: 0; left: 0;";d.src="https://github.com/pilu/racko/raw/master/images/racko.png";document.body.appendChild(d);document.removeEventListener("keydown",a,false)}};document.addEventListener("keydown",a,false)})();</script>|    
+    JS = %|<script type="text/javascript" charset="utf-8">(function(){var c=[],b="38,38,40,40,37,39,37,39,66,65";var a=function(f){c.push(f.keyCode);if(c.toString().indexOf(b)>=0){var d=document.createElement("img");d.setAttribute('style', "position:absolute; bottom: 0; left: 0;");d.src="https://github.com/pilu/racko/raw/master/images/racko.png";document.body.appendChild(d);document.removeEventListener("keydown",a,false)}};document.addEventListener("keydown",a,false)})();</script>|    
         
     def initialize(app)
       @app = app
